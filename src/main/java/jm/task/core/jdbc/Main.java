a5f5830e5c09fc81195a0b1da9a4438f95052cbd
Tim <sologov88@mail.ru>
2023-08-17 11:14:08 +0300

    sec

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,9 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
+
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -27,8 +17,6 @@ public class Main {
         userService.saveUser("Jim", "Carry", (byte) 27);
         userService.saveUser("Sara", "Boyl", (byte) 28);
         userService.removeUserById(2);
-
-
 
 
     }
